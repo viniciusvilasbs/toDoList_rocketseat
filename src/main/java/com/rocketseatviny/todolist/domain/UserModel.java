7823b3c22f2d995d535fee3b10be8800e45727a9
viniciusvilasbs <vinyvilas@gmail.com>
2023-10-17 10:08:29 -0300

    Add User domain

---
+++ src/main/java/com/rocketseatviny/todolist/domain/UserModel.java
@@ -0,0 +1,30 @@
+package com.rocketseatviny.todolist.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
